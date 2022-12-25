43011dff7031a02c0a5068596cf51b852715911a
baribgf <bougafa.005@gmail.com>
2022-12-25 02:45:42 +0100

    .

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,29 @@
+=begin
+    set(path, data, query_options)
+    get(path, query_options)
+    push(path, data, query_options)
+    delete(path, query_options)
+    update(path, data, query_options)
+=end
+
 class MainController < ApplicationController
+  def initialize
+    base_uri = 'https://bari-example-default-rtdb.europe-west1.firebasedatabase.app/'
+    firebase_db_key = File.open('firebase-db-key.json').read
+    @firebase = Firebase::Client.new(base_uri, firebase_db_key)
+    @lang_db_name = "langs"
+  end
   def index
-    @langs = Lang.all
+    @langs = {}
+    n = 0
+    begin
+      for i in @firebase.get(@lang_db_name).body do
+        n += 1
+        i[0] = n
+        @langs.store(i[0], i[1])
+      end
+    rescue => e
+    end
   end
 
   def new
@@ -9,7 +32,7 @@ class MainController < ApplicationController
     
     begin
       if @lang.length > 0 and @year.length > 0
-        Lang.create(name: @lang, year: @year)
+        @firebase.push(@lang_db_name, {name: @lang, year: @year})
         render plain: "OK"
       end
     rescue => e
@@ -21,8 +44,8 @@ class MainController < ApplicationController
   def delete
     id = params[:id]
    begin
-    status = Lang.delete(id)
-    if status != 1
+    status = @firebase.delete(@lang_db_name + "/" + @firebase.get(@lang_db_name).body.keys[id.to_i - 1])
+    if status.code != 200
       raise
     end
     render plain: "OK"
